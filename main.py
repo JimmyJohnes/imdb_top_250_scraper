c8b9771c41682204a07215980618313ca74e966d
Adham <amsoliman03@outlook.com>
2024-05-10 14:36:09 +0300

    finished scraping entirety of imdb top 250

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import requests
 import pandas as pd
 import re
 from bs4 import BeautifulSoup
-#from selenium import webdriver
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.chrome.service import Service
-#from webdriver_manager.chrome import ChromeDriverManager
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
+import json
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -32,7 +27,7 @@ def is_in_ordered_list(key: str)->bool:
     return bool(re.search(r'^\d',key))
 
 def is_a_year_format(key: str)->bool:
-    """ returns if a given key is in the year format {\d\d\d\d}"""
+    """ returns if a given key is in the year format {\d \d \d \d}"""
     return bool(re.search(r'\d\d\d\d',key))
 
 def get_top_250_movies_titles(soup)->list[str]:
@@ -44,7 +39,7 @@ def get_top_250_movies_titles(soup)->list[str]:
 def get_top_250_movies_ratings(soup)->list[str]:
     """ given the BeautifulSoup html object, returns all 250 ratings of the listed movies"""
     ratings = soup.find_all('span', class_='ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating')
-    ratings = [item.text.replace("\xa0"," ") for item in ratings]
+    ratings = [item.text.replace("\xa0"," ").split(" ")[0] for item in ratings]
     return ratings 
 
 def get_top_250_movies_release_years(soup)->list[str]:
@@ -75,6 +70,75 @@ def get_top_250_movies_genres(links,headers):
         genres.append(','.join(data))
     return genres
 
+def get_top_250_movies_images(soup) -> list[str]:
+    images = soup.find_all('img', class_="ipc-image")
+    for index in range(len(images)):
+        images[index] = images[index]["src"]
+    return images
+
+def get_top_250_movies_directors(links, headers):
+    """ given the top 250 movie links, returns a 2d list of all the directors of each movie """
+    directors = []
+    for link in links:
+        title = str(link).split('/')[MOVIE_TITLE]
+        response = requests.request("GET", f"https://m.imdb.com/title/{title}", headers=headers, data={})
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text,"lxml")
+        #
+        # find the script tag that contains the word "Director"
+        #
+        check = re.compile("Director")
+        soup = [s for s in soup.find_all('script') if re.search(check,s.text)]
+        soup = BeautifulSoup(soup[0].text,"lxml")
+        decoder = json.JSONDecoder()
+
+        try:
+            soup = decoder.decode(soup.text)
+        except:
+            soup = '{' + soup.text[soup.text.find("\"director\""):]
+            soup = decoder.decode(soup)
+
+        directors_for_this_movie = []
+        if "props" in soup.keys():
+            directors_for_this_movie = soup["props"]["pageProps"]["mainColumnData"]["directors"]
+            directors_for_this_movie = [director_name["name"]["nameText"]["text"] for director_name in directors_for_this_movie[0]["credits"]]
+        else:
+            directors_for_this_movie = soup["director"]
+            directors_for_this_movie = [director["name"] for director in directors_for_this_movie]
+        directors.append(','.join(directors_for_this_movie))
+    return directors
+
+def get_top_250_movies_cast(links, headers):
+    """ given the top 250 movie links, returns a 2d list of all the directors of each movie """
+    cast = []
+    for link in links:
+        title = str(link).split('/')[MOVIE_TITLE]
+        response = requests.request("GET", f"https://m.imdb.com/title/{title}", headers=headers, data={})
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text,"lxml")
+        #
+        # find the script tag that contains the word "cast"
+        #
+        check = re.compile("cast")
+        soup = [s for s in soup.find_all('script') if re.search(check,s.text)]
+        soup = BeautifulSoup(soup[0].text,"lxml")
+        decoder = json.JSONDecoder()
+
+        try:
+            soup = decoder.decode(soup.text)
+        except:
+            soup = '{' + soup.text[soup.text.find("\"actor\""):]
+            soup = decoder.decode(soup)
+
+        cast_for_this_movie = []
+        if "props" in soup.keys():
+            cast_for_this_movie = soup["props"]["pageProps"]["mainColumnData"]["cast"]
+            cast_for_this_movie = [cast_name["node"]["name"]["nameText"]["text"] for cast_name in cast_for_this_movie["edges"]]
+        else:
+            cast_for_this_movie = soup["actor"]
+            cast_for_this_movie= [cast["name"] for cast in cast_for_this_movie]
+        cast.append(','.join(cast_for_this_movie))
+    return cast
 
 def scrape_movie_data(base_link, chart_link,headers):
 
@@ -91,88 +155,28 @@ def scrape_movie_data(base_link, chart_link,headers):
 
     genres = get_top_250_movies_genres(links,headers)
 
+    images = get_top_250_movies_images(soup)
+    images.pop()
     
-    #print([{
-    #    "name": titles[index],
-    #    "genre": genres[index]
-    #    } for index in range(len(genres))])
+    directors = get_top_250_movies_directors(links,headers)
+    
+    cast = get_top_250_movies_cast(links,headers)
 
-#    genre_div = soup.find('div', {'class': 'ipc-chip-list__scroller'})
-#    genres = [chip.find('span', {'class': 'ipc-chip__text'}).get_text() for chip in genre_div.find_all('a')]
-#
-#    year_div = soup.find('div', class_="sc-b7c53eda-0 dUpRPQ")
-#    year_link = year_div.find('a', class_="ipc-link ipc-link--baseAlt ipc-link--inherit-color")
-#    year = year_link.text
-#
-#    try:
-#        directors_ul = soup.select_one('span:contains("Directors") + div ul').find_all('li')
-#        directors = [director.text for director in directors_ul]
-#    except:
-#        directors = [soup.find('a', class_="ipc-metadata-list-item__list-content-item "
-#                                           "ipc-metadata-list-item__list-content-item--link").text]
-#
-#    cast_links = soup.find_all('a', {'data-testid': 'title-cast-item__actor'})
-#    cast = [actor.get_text() for actor in cast_links]
-#
-#    first_link = WebDriverWait(driver, 10).until(
-#        EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.ipc-lockup-overlay.ipc-focusable'))
-#    )
-#
-#    first_link.click()
-#
-#    link_for_img = driver.find_element(By.CSS_SELECTOR, 'a[data-testid="mv-gallery-button"]').get_attribute('href')
-#
-#    second_link = WebDriverWait(driver, 10).until(
-#        EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[data-testid="mv-gallery-button"]'))
-#    )
-#
-#    second_link.click()
-#
-#    response = requests.get(url=f'{link_for_img}', headers=HEADERS)
-#    response.raise_for_status()
-#
-#    soup = BeautifulSoup(response.text, 'html.parser')
-#    img_src = soup.find('img', class_='poster').get('src')
-#
     movie_data = {
         "titles": titles,
         "ratings": ratings,
         "genres": genres,
         "release_years": years,
-#        "director(s)": directors,
-#        "cast": cast,
-#        "image": img_src
+        "director(s)": directors,
+        "cast": cast,
+        "image": images,
+        "link": [base_link + link for link in links]
     }
-#
     return movie_data
-#
-#
-#chrome_options = webdriver.ChromeOptions()
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-#
-#try:
-#    driver.get(URL)
-#
-#    movies = driver.find_elements(By.CSS_SELECTOR, "a.ipc-title-link-wrapper h3.ipc-title__text")
-#    movie_links = [movie.find_element(By.XPATH, "./ancestor::a").get_attribute('href') for movie in movies]
-#
-#    for link in movie_links[:251]:
-#        driver.get(link)
-#        # time.sleep(0.25)
-#
-#        current_movie_data = scrape_movie_data(link)
-#        MOVIES_DATA.append(current_movie_data)
-#        # time.sleep(0.25)
-#
-#        driver.back()
-#
-#except Exception as e:
-#    print(f'Error has occurred: {e}')
-#
-#finally:
-#    df = pd.DataFrame(MOVIES_DATA)
-#    df.to_csv("imdb_top_250_movies_dataset.csv")
-#    driver.quit()
-#
+
+start = time.time()
+print("Starting the Scrapin")
 data = scrape_movie_data(BASE_URL,TOP_MOVIES_URL,HEADERS)
-print(pd.DataFrame(data))
+print("Scraping ended, time taken:",time.time() - start)
+data = pd.DataFrame(data)
+data.to_csv("data.csv")
